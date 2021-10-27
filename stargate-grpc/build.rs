cd6fa749c7f83b8a18c85cb2c111d923ee524680
Piotr Kołaczkowski <pkolaczk@datastax.com>
2021-10-27 10:50:47 +0200

    Move README.md under crate dir
    Unfortunately having the README.md outside of the
    crate dir caused a conflict between skeptic
    (README testing) and crate publishing.

--- stargate-grpc/build.rs
+++ stargate-grpc/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("api/stargate.proto")?;
-    skeptic::generate_doc_tests(&["../README.md"]);
+    skeptic::generate_doc_tests(&["README.md"]);
     Ok(())
 }
